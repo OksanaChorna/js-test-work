638b5c547e9d37047260aa055662358912eb1e4a
Oksana Chorna <ksuchorna@gmail.com>
2021-04-06 14:21:50 +0300

    вотс

---
+++ slider.js
@@ -0,0 +1,22 @@
+// function onLeftArrow(event) {
+//   //   if (event.code === "ArrowLeft") {
+//   //     lightboxImgRef
+//   //   }
+//   galleryRef.();
+// }
+
+// const img = (img) => {
+//   return galleryRef.map((img) => galleryRef.img);
+// };
+// }
+function onRightArrow(event) {
+  if (event.code === 'ArrowRight') {
+    // знаходимо поточну картинку і її індекс
+    // потім її індекс +1
+    //
+  }
+}
+// const getUserNames = (users) => {
+//   // твой код
+//   return users.map((user) => user.name);
+// };
